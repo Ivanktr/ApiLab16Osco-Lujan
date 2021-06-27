5818e6f14136ef995fb15e90fc87da84b156ffd5
Ivan <51338600+Ivanktr@users.noreply.github.com>
2021-06-26 23:34:10 -0500

    Agregar archivos de proyecto.

---
+++ TodoREST/Constants.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TodoREST
+{
+    public static class Constants
+    {
+        // URL of REST service
+        public static string RestUrl = Device.RuntimePlatform == Device.Android ? "http://127.0.0.1:3000/personas/{0}" : "http://localhost:3000/personas/{0}";
+        //public static string RestUrl = "http://localhost:3000/personas/{0}";
+
+    }
+}
---
+++ TodoREST/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TodoREST.Models
+{
+	public class TodoItem
+	{
+		public string ID { get; set; }
+
+		public string Name { get; set; }
+
+		public string Age { get; set; }
+
+		public string Weight { get; set; }
+
+		public string BirthdayDate { get; set; }
+	}
+}
